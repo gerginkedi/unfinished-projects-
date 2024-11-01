456f6e3587bb021362080fc5541b44b74fb40d30
kedi <98282468+gerginkedi@users.noreply.github.com>
2024-11-02 01:07:48 +0300

    vuaah
    vuaahvuaahvuaah

---
+++ pigeon test/pigeon test/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace pigeon_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(281, 104);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(207, 216);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "are you pigeon";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+    }
+}
